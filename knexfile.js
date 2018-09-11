40e2f84bbce0caf3f3537fd73082b7ff404695cb
Raldes krisnu <raldeskrisnu@gmail.com>
2018-09-12 00:14:38 +0700

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,9 @@
+const env = require('dotenv').config({path: '.env'});
+
 module.exports = {
     development: {
         client: 'postgresql',
-        connection: `{${process.env.DATABASE_URL}}`,
+        connection: `postgres://localhost:5432/${process.env.DATABASE_NAME}`,
         migrations: {
             directory: __dirname + '/src/database/migrations'
         },
