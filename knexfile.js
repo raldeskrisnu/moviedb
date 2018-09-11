80e1655e681d744b91a91f765bc70960e199f45e
Raldes krisnu <raldeskrisnu@gmail.com>
2018-09-11 23:39:55 +0700

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         client: 'postgresql',
-        connection: `postgres://localhost:5432/${process.env.DATABASE_NAME}`,
+        connection: `{${process.env.DATABASE_URL}}`,
         migrations: {
             directory: __dirname + '/src/database/migrations'
         },
