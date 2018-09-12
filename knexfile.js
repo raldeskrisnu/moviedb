213de23c65cec230617a15e406998904d9133ef8
Raldes krisnu <raldes.krisnu@myrepublic.net>
2018-09-12 16:09:45 +0700

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const env = require('dotenv').config({path: '.env'});
 module.exports = {
     development: {
         client: 'postgresql',
-        connection: `{${process.env.DATABASE_URL}}`,
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/src/database/migrations'
         },
@@ -13,7 +13,7 @@ module.exports = {
     },
     production: {
         client: 'postgresql',
-        connection: `{${process.env.DATABASE_URL}}`,
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/src/database/migrations'
         },
